1088f9c8ed8f75b79a5802128f763a01775e41bb
Daniel Nicodemo <dan.nicodemo@gmail.com>
2015-11-30 16:17:22 -0500

    updated things I think

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
 Rails.application.routes.draw do
   
   #REGISTRATION CONTROLLER
-post "/signup", to: "registrations#create"
-delete "/signup", to: "registrations#destroy"
-post "/login", to: "registrations#login"
-get "/logout", to: "registrations#logout"
+post "/signup", to: "registrations#create"  #creates a new user
+delete "/signup", to: "registrations#destroy" #deletes a user
+post "/login", to: "registrations#login"  #login user
+get "/logout", to: "registrations#logout" #logout user
+
+  #USERS CONTROLLER
+#Get User Profile/Calendar
+#Update User Profile (name, email, etc)
+#Add Calendar Event
+#Edit Calendar Event
+#Delete Calendar Event
+#Push events to group calendar
+
+
 
 end
