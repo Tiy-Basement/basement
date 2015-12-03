d31e5ae774eec2c8815452f8d38d115703953833
Daniel Nicodemo <dan.nicodemo@gmail.com>
2015-12-03 13:17:18 -0500

    users controller tweaks

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = Users.all
+  end
+
+  def event_index
+    @user = User.find(id: current_user.id)
+    @user.events.all
+  end
 
 end
