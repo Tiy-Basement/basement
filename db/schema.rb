eaa1c704ac9e073de5ab2e46eb4561a95b8790b0
Don Deckard <don_deckard@yahoo.com>
2015-12-07 11:50:02 -0500

    stuff

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151202194337) do
+ActiveRecord::Schema.define(version: 20151207150953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,18 @@ ActiveRecord::Schema.define(version: 20151202194337) do
   create_table "cookiejars", force: :cascade do |t|
     t.string   "cookie"
     t.integer  "used"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "start"
+    t.datetime "end"
+    t.string   "location"
+    t.string   "note"
+    t.integer  "user_id"
+    t.integer  "group_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
