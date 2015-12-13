7ae190226c4dbd9e414c906eb8c34a01096f9ce3
Don Deckard <don_deckard@yahoo.com>
2015-12-13 15:59:18 -0500

    trying out some better error handling in the user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,16 +5,20 @@ class UsersController < ApplicationController
     render "index.json.jbuilder", status: :ok
   end
 
-  def groups
+  def groups #user to retrieve all of a users groups
 		@groups = current_user.groups.order("name")
 		render "groups.json.jbuilder", status: :ok
 
    end
 
-  def user_info
-    @user = User.where(id: params[:id])
-    @groups = current_user.groups.order("name")
-    render "user.json.jbuilder" , status: :ok
+  def user_info #used to retrieve a users info plus all of their groups
+    begin
+      @user = User.where(id: params[:id])
+      @groups = current_user.groups.order("name")
+      render "user.json.jbuilder" , status: :ok  
+    rescue => e
+      Rails.logger.error { "#{e.message} #{e.backtrace.join("\n")}" }
+    end
   end
 end
 
