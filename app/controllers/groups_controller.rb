4a4abb4c46df28a4af8b848e81ba81340ce48ee0
Daniel Nicodemo <dan.nicodemo@gmail.com>
2015-12-14 11:20:36 -0500

    fixed jointable

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -12,7 +12,7 @@ class GroupsController < ApplicationController
 		end
 		#todo: if group join_password is given set public to false
 		#binding.pry
-		@member = Groups_user.create(user_id: current_user.id, 
+		@member = current_user.groups.create(user_id: current_user.id, 
 																group_id: @group.id)
 
 		if @group.save
