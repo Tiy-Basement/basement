d13475c82f31a258bdfc9f6a3c82e24bf5eeab5a
Don Deckard <don_deckard@yahoo.com>
2015-12-14 15:45:42 -0500

    adding nicer response codes to membership controller

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -5,18 +5,18 @@ class MembershipsController < ApplicationController
   		@member = User.find_by(username: params[:username])
   		@group = Group.find_by(name: params[:name])
   		@member.groups << @group
-		render "member.json.jbuilder", status: :ok
+		render "member.json.jbuilder", status: :created
 	end
 
 	def remove
     	@member = current_user.members.find_by(user_id: params[:user_id],group_id: params[:group_id])
 		@member.destroy
-		render "killmember.json.jbuilder", status: :ok
+		render "killmember.json.jbuilder", status: :removed_permanently
 	end
 
 	def memberindex
 	    @members = Groups_Users.all
-	    render  "memberindex.json.jbuilder", status: :ok
+	    render  "memberindex.json.jbuilder", status: :found
 	end
 		
 end
