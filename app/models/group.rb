dfc05651af378467ed3c67edc439a3069589d0f7
Don Deckard <don_deckard@yahoo.com>
2015-12-16 11:16:24 -0500

    added dependant destroy to models

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,7 @@
 class Group < ActiveRecord::Base
   has_many :groups_users
   has_many :users, through: :groups_users
-  has_many :events
+  has_many :events ,dependent: :destroy
   # has_many :member_events, through: :users,
   #   source: :events
 end
